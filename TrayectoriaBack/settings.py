136ebe7a115242c95c82f200e19a7b8c74133d22
FranciscoErick0135 <193241@ids.upchiapas.edu.mx>
2020-11-06 00:04:12 -0600

    gitignore

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -101,27 +101,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TrayectoriaBack.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME':'trayectoriadb',
-#        'USER':'postgres',
-#        'PASSWORD':'012601',
-#        'HOST':'127.0.0.1',
-#        'PORT': '5432'
-#    }
-#}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
